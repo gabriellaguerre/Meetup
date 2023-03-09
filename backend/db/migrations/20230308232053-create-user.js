2751bd408754186e3cadaa0d33360a5105f236b9
gabriellaguerre <laguerre.g.g@gmail.com>
2023-03-09 12:41:22 -0500

    modified migration to include option

--- backend/db/migrations/20230308232053-create-user.js
+++ backend/db/migrations/20230308232053-create-user.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -42,9 +48,10 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
 
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
   }
 };
