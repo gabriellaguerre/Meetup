b0677ef84016379c1cbc58f12ca56ba58ede03b3
gabriellaguerre <laguerre.g.g@gmail.com>
2023-03-14 19:00:34 -0400

    created Memberships join table

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -41,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
     return await User.scope('currentUser').findByPk(user.id);
   }
     static associate(models) {
-      // define association here
+      User.belongsToMany(models.Group, {through: models.Membership})
     }
   }
   User.init({
