969d07459ae924d7a09e46ce4e43eed7b4252ee2
gabriellaguerre <laguerre.g.g@gmail.com>
2023-03-31 20:16:09 -0400

    added seed data to get postgresql running

--- backend/db/seeders/20230314234022-demo-group.js
+++ backend/db/seeders/20230314234022-demo-group.js
@@ -11,13 +11,13 @@ module.exports = {
     options.tableName = 'Groups';
     return queryInterface.bulkInsert(options, [
       {
-//       name: "Evening Tennis on the Water",
-//       about: "Enjoy rounds of tennis with a tight-nit group of people on the water facing the Brooklyn Bridge. Singles or doubles.",
-//       type: "In person",
-//       private: true,
-//       city: "New York",
-//       state: "NY"
-//    },
+      name: "Evening Tennis on the Water",
+      about: "Enjoy rounds of tennis with a tight-nit group of people on the water facing the Brooklyn Bridge. Singles or doubles.",
+      type: "In person",
+      private: true,
+      city: "New York",
+      state: "NY"
+   },
 //    {
 //       name: "Swimming across Lake Cool-Waters",
 //       about: "Come join us in swimming across this fantastic lake. Singles or doubles.",
@@ -33,14 +33,14 @@ module.exports = {
 //       private: true,
 //       city: "New York",
 //       state: "NY"
-    }])
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
     options.tableName = 'Groups';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      name: { [Op.in]: ['Evening Tennis on the Water', 'Swimming across Lake Cool-Waters', 'Chess mini championship'] }
+      name: { [Op.in]: ['Evening Tennis on the Water'] }
     }, {});
   }
 };
